a23e9ddf41990ab431e3ebcce65a40b2432957f7
Dmitriy Bogun <surabujin@surabujin.org.ua>
2020-06-11 17:03:09 +0300

    Preparations for pypi.org publication

--- resta/endpoint.py
+++ resta/endpoint.py
@@ -1,9 +1,20 @@
-# endpoint.py
+#  Copyright (c) 2020 Dmitry Bogun. Contacts: <nayka@nyaka.org>
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
 
 
 class Endpoint(object):
-    def __init__(self, base_url, session_provider, context):
-        self.base_url = base_url
+    def __init__(self, session_provider, context=None):
         self.session_provider = session_provider
         self.context = context
 
@@ -12,7 +23,7 @@ class Endpoint(object):
 
     def _handle(self, request):
         self._inject_headers(request)
-        request.emit(self.session_provider())
+        return request.emit(self.session_provider())
 
     def _inject_headers(self, request):
         pass
--- setup.py
+++ setup.py
@@ -25,9 +25,18 @@ if {'pytest', 'test', 'ptr'}.intersection(sys.argv):
 setup(
     name='RESTa',
     version=resta.__version__,
+    author='Dmitry Bogun',
+    author_email='nyaka@nyaka.org',
     description='REST API client framework',
     long_description=open(path('README.md')).read(),
     long_description_content_type='text/markdown',
+    url='https://github.com/surabujin/resta',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Development Status:: 1 - Planning'
+    ],
     packages=find_packages(),
     setup_requires=setup_deps,
     tests_require=test_deps,
